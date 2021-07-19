2466d9e1b783fababb84a07c487de6c8a0732771
DaijiroYamazaki <d-yamazaki@tkk-works.co.jp>
2021-07-19 13:39:36 +0900

    テスト環境に合わせて設定変更、2番機のみ地図表示

--- 11th/start_5.sh
+++ 11th/start_5.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
-AP_HOME=/home/tsuyoshi/GitHub/tajisoft/ardupilot
+AP_HOME=/mnt/d/workspace/ardupilot
 
 screen -S mav1 -dm $AP_HOME/Tools/autotest/sim_vehicle.py -v ArduCopter -I0 --custom-location=35.79196857,139.0513938,0,0 --sysid 1
 sleep 1
-screen -S mav2 -dm $AP_HOME/Tools/autotest/sim_vehicle.py -v ArduCopter -I1 --custom-location=35.80005627,139.0842228,0,0 --sysid 2
+screen -S mav2 -dm $AP_HOME/Tools/autotest/sim_vehicle.py -v ArduCopter -I1 --custom-location=35.80005627,139.0842228,0,0 --map --console --sysid 2
 sleep 1
 screen -S mav3 -dm $AP_HOME/Tools/autotest/sim_vehicle.py -v ArduCopter -I2 --custom-location=35.80801586,139.0705794,0,0 --sysid 3
 sleep 1
